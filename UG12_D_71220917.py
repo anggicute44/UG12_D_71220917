900041f75e24c1bc85c6e6f954126aed4be3e8a0
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2022-12-08 15:55:04 +0700

    Add files via upload

---
+++ UG12_D_71220917.py
@@ -0,0 +1,19 @@
+
+#print("pilihan model matematika")
+#print("1.perkalian")
+#print("2. pembagian")
+print("masukkan model matematika yang di ingginkan ")
+num = int(input("menampilkan tabel matematika dengan angka: "))
+
+for i in range(1,11):   
+    print(num,'x',i,'=', num*i) 
+
+#3
+print("pilihan model matematika")
+print("1.perkalian")
+print("2.pembagian")
+num = int(input("massukan model matematika yang di inginkan 1/2"))
+num = int(input("menampilkan table matematika dengan angka"))
+
+for i in range(50,66):   
+    print(num,'x',i,'=', num/i)
